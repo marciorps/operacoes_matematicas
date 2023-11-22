3c39667c3f9b675495b76a9c12acbd0e5a9a981c
marciorps <marciorps@yahoo.com,.br>
2023-11-22 15:46:31 -0300

    operadores

---
+++ oper_matematica.py
@@ -0,0 +1,30 @@
+a = 20
+b = 20.5
+print(type(a))
+print(type(b))
+#Operações Matemáticas
+print(10 + 6)
+print(10 - 6)
+print(10 * 6)
+print(10 / 6)
+print(10 // 6) #divisão de inteiro
+print(10 % 6) #Modulus
+print(10 ** 6) #exponencial
+
+
+#Atalho para atribuição mais rápida
+
+a = 10
+a = a + 5
+a += 5
+
+b = 20
+b = b - 2
+b -= 2
+
+# Operações matemáticas comuns(arrendodamento)
+print(round(5.7))
+import math
+print(round(5.7))
+print(math.ceil(2.2))
+
